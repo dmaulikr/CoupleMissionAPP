68e61468cd7d09286576db55f2b2286df0c5a056
SangMin,Yeo <devssgg@gmail.com>
2017-03-08 22:58:06 +0900

    Add Implementation Randmon Method

--- Hackerthon/Hackerthon/ViewController.m
+++ Hackerthon/Hackerthon/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic) NSString *txt;
+@property (nonatomic) UILabel *label;
+@property (nonatomic) NSUInteger num;
 @end
 
 @implementation ViewController
@@ -17,8 +19,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    랜덤버튼
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(100, 300, self.view.frame.size.width*30/100, self.view.frame.size.height*20/100)];
+    [btn setTitle:@"Random" forState:UIControlStateNormal];
+    [btn setTitle:@"Random" forState:UIControlStateHighlighted];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+    btn.layer.borderWidth = 1;
+    btn.layer.borderColor = [UIColor blackColor].CGColor;
+    [btn addTarget:self action:@selector(btnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+//    숫자 Label
+    self.label = [[UILabel alloc]initWithFrame:CGRectMake(100, 150, self.view.frame.size.width*30/100, self.view.frame.size.height*20/100)];
+    self.label.text = @"버튼 눌러";
+    self.label.textColor = [UIColor blackColor];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.label];
 }
 
+- (void)btnClicked:(UIButton *)sender
+{
+    int random = arc4random() % (50);
+    NSString *temp = [NSString stringWithFormat:@"%d",random];
+    NSLog(@"버튼클릭");
+    self.label.text = temp;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
